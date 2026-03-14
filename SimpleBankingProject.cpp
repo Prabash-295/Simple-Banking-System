281907010ad593d7dab5a5f8e97140df4de742d5
Prabash-295 <prabashvarmamandapati@gmail.com>
2026-03-15 00:09:44 +0530

    Spelling mistake corrected

--- SimpleBankingProject.cpp
+++ SimpleBankingProject.cpp
@@ -79,7 +79,7 @@ int main() {
             myAccount.displayAccountInfo();
             break;
         case 4:
-            cout << "Exiting..." << endl;
+            cout << "Exited" << endl;
             break;
         default:
             cout << "Invalid choice. Please try again." << endl;
